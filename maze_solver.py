cb35e7842054ba30f1430e856d3858735dd54574
jonathanalaksana <jonathanalaksana@gmail.com>
2021-08-12 12:24:33 -0700

    Add main program and introduction

--- maze_solver.py
+++ maze_solver.py
@@ -1,38 +1,39 @@
 """
 Maze Solver
 Started 8/4/21
-Finished 
+Finished 8/12/21
 
 Goal: To take in an image of a black and white maze and to find and mark
-the shortest path from start to end.
+the path from start to end.
+
+I have always loved mazes as a kid. One of the reasons I became interested in computer
+science is a computer's ability to solve mazes on its own. In this project, I built a program
+that solves a maze (with the constraints written below) with a selection for four different
+path finding algorithms: DFS, BFS, Dijkstra, and A Star. I was inspired to finally make this project
+after watching Computerphile's video on maze solving by Dr. Mike Pound. 
 
 Maze constraints:
+ - Maze is a png image. Does not need to be square
  - black and white pixels where white is paths
  - surrounded by black walls
  - one white at top is entrance, one white at bottom is exit
  - maze can have multiple solutions
 
-
-Control Flow:
-1. Takes in a png file of a maze
-2. Parse through pixels
-3. Create a node at every junction and dead end
-4. Connect nodes to adjacent nodes to create a graph
-5. Implement breadth first search
-6. Implement Dijkstra algorithm
-7. Implement a* algorithm
-8. Convert solutions from algorithm to draw solution onto maze
+Instructions:
+ 1. Have Python 3.6 or later and the Pillow module installed
+ 2. Put image of maze in the mazes folder
+ 3. Run the file
+ 4. Input the name of the file. Wait for program to parse the maze
+ 5. Input the name of the algorithm you want to use (dfs, bfs, dijkstra, or astar)
+ 6. Wait for program to solve.
+ 7. Open solution file from solutions folder to view solution.
 
 Notes:
- - Dijkstra and a* need a priority queue
- - learn how to store png data
-
-
- Week One Goal: Parse the maze image and create a graph representing the maze   COMPLETE
- Week Two: Implement the searching algorithms   COMPLETE
- Week Three: Tie up the components and create output
-
-
+ - This project was inspired and adapted from Dr. Mike Pound's version.
+ - All test mazes were downloaded from Dr. Mike Pound's version. Github link below
+ - DFS and BFS generally performs faster than Dijkstra and A Star but also do not give the shortest path
+ - The combo6k maze best showcases the differenct between DFS and A Star
+ - Not all solutions for every algorithm for every maze are included in this repository
 
 Resources:
 https://www.youtube.com/watch?v=rop0W4QDOUI&list=PLpxeghl7_0IXWkeE8K-XGl2YRxqTo5IJL&index=9
@@ -42,12 +43,13 @@ https://github.com/mikepound/mazesolving
 
 """
 
+from typing import List, Set
 from PIL import Image
 from maze import Maze
 import time
 
 
-def save_output(im, path, output):
+def save_output(im, path: List, output: str) -> None:
     """Draws the solution path onto an output file of the maze.
     Params: image object, solution list of vertices, name of output file"""
     im = im.convert("RGB")
@@ -70,16 +72,63 @@ def save_output(im, path, output):
 
     im.save(output)
 
+def ask_question(prompt: str, acceptable_reponses: Set) -> str:
+    """Asks user to input an answer based on a prompt. Exits program if there are three bad attempts.
+    Params: Question prompt and set of acceptable responses.
+    Returns the inputted answer if acceptable"""
+    answer = input(prompt)
+    
+    bad_ans = 0
+    while not(answer.lower() in acceptable_reponses):
+        bad_ans += 1
+        if bad_ans == 3:
+            print('Sorry, we cannot help you here.')
+            quit()
+      
+        print('Can you please try again:')
+        answer = input(prompt)            
+        
+    return answer
 
+def main() -> None:
+    print("-----Maze Solver-----\n")
+    
+    im = None
+    while im is None:
+        png = input("Enter name of maze: ")
+        try:
+            im = Image.open("mazes/" + png)
+        except:
+            print("Invalid image file name. Try again")
+    
+    
+    print("Parsing maze...")
+    t0 = time.time()
+    try:
+        maze = Maze(im)
+    except:
+        print("Error: Could not parse maze. Check validity of maze.")
+        quit()
+    t1 = time.time()
+    print("Finished parsing in %2.5f seconds"%(t1-t0))
 
-im = Image.open("mazes/tiny.png")
-print("Parsing maze...")
-maze = Maze(im)
-print("Solving maze...")
-t0 = time.time()
-path = maze.bfs()
+    alg = ask_question("What algorithm do you want to use? ", set(["dfs", "bfs", "dijkstra", "astar"]))
+    print("Solving maze...")
+    t0 = time.time()
+    if alg == "dfs":
+        path = maze.dfs()
+    elif alg == "bfs":
+        path = maze.bfs()
+    elif alg == "dijkstra":
+        path = maze.dijkstra()
+    else:
+        path = maze.astar()
+    t1 = time.time()
+    print("Finished solving in %2.5f seconds"%(t1-t0))
 
-t1 = time.time()
-print("Finished in %2.5f seconds"%(t1-t0))
-print("Saving image...")
-save_output(im,path,"solutions/tiny.png")
+    print("Saving image...")
+    save_output(im,path,"solutions/" + png[:-4] + "_sol_" + alg.lower() + ".png")
+    print("End of program.\n")
+
+if __name__ == "__main__":
+    main()
