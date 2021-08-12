b70374f75e34e0fb19c15c0c6597a8452dfa618c
jonathanalaksana <jonathanalaksana@gmail.com>
2021-08-12 12:23:34 -0700

    Changed dfs from recusive to iterative

--- maze.py
+++ maze.py
@@ -1,12 +1,14 @@
 from collections import deque
-from typing import List
+from typing import List, Any
 from priorityqueue import PriorityQueue
+from PIL import Image
 import math
+
 
 class Maze:
     """Maze Class
     Creates a graph representation of a maze image"""
-    def __init__(self, im) -> None:
+    def __init__(self, im: Image) -> None:
         """Param: image object"""
         pixels = list(im.getdata(0))
         width, height = im.size
@@ -89,6 +91,7 @@ class Maze:
                     
                     self.num_items += 1
 
+        # Find end
         for x in range(1,width-1):
             if pixels[height-1][x] == 1:
                 self.end = (x,height-1)
@@ -100,11 +103,11 @@ class Maze:
         self.width = width
         self.height = height
 
-    def addVertex(self, vertex):
+    def addVertex(self, vertex: tuple) -> None:
         """Adds given vertex key to adjacency list. Sets default value to an empty dictionary"""
         self.adjacencyList[vertex] = {}
 
-    def addEdge(self, source, dest):
+    def addEdge(self, source: tuple, dest: tuple):
         """Adds an edge to the source and destination vertexes. Calculates the weight and 
         updates adjacency list. Assumes both vertexes are already in the graph"""
         dx = source[0] - dest[0]
@@ -113,36 +116,10 @@ class Maze:
         self.adjacencyList[source][dest] = weight
         self.adjacencyList[dest][source] = weight
 
-    def dfs(self) -> List:
-        """Depth-First Search algorithm.
-        Returns the solution path a list of vertices using backtracking
-        O(V+E) time"""
-        return self.dfs_helper(self.start, set(), [])
-
-    def dfs_helper(self, cur, visited, path) -> List:
-        """DFS recursion helper
-        Param: current vertex, visited set, path list"""
-        visited.add(cur)
-        path.append(cur)
-
-        if cur == self.end:
-            return path
-
-        res = None
-        for vert in self.adjacencyList[cur].keys():
-            if vert not in visited:
-                res = self.dfs_helper(vert, visited, path)
-
-        if res is None:
-            path.pop()
-            return
-        
-        return res
-
     def _backtrace(self, table: dict) -> List:
         """Takes a dictionary with node keys and its previous node as a value.
         Returns a list of vertices in the order of the solution path
-        O(n) time where n is the number of items in the table"""
+        O(n) time where n is the number of items in the solution path"""
         front = self.end
         res = deque()
         res.append(self.end)
@@ -150,6 +127,31 @@ class Maze:
             res.appendleft(table[front])
             front = res[0]
         return res
+
+    def dfs(self) -> List:
+        """Depth-First Search algorithm.
+        Returns the solution path a list of vertices
+        Opted for iterative over recursive implementation because larger mazes
+        often exceed recursion limit
+        O(V+E) time"""
+        stack = []
+        visited = set()
+        prev ={}
+
+        stack.append(self.start)
+        while stack:
+            cur = stack.pop()
+            visited.add(cur)
+
+            if cur == self.end:
+                break
+
+            for vert in self.adjacencyList[cur].keys():
+                if vert not in visited:
+                    stack.append(vert)
+                    prev[vert] = cur
+
+        return self._backtrace(prev)                  
 
     def bfs(self) -> List:
         """Breadth-First Search algorithm.
@@ -205,6 +207,7 @@ class Maze:
         Returns a list of vertices in order of the solution path
         O(ElogV), generally performs better than Dijkstra"""
         dist = {v:math.inf for v in self.adjacencyList.keys()}
+        heur = {}
         source = dict()
         visited = set()
         pq = PriorityQueue(self.num_items*2) 
@@ -221,7 +224,9 @@ class Maze:
                 if vert not in visited and dist[vert] > dist[u] + weight:
                     dist[vert] = dist[u] + weight
                     source[vert] = u
-                    pq.enqueue(vert, dist[vert] + self.manhattan_dist(vert, self.end))
+                    if vert not in heur:
+                        heur[vert] = self.manhattan_dist(vert, self.end)
+                    pq.enqueue(vert, dist[vert] + heur[vert])
         
         return self._backtrace(source)
 
